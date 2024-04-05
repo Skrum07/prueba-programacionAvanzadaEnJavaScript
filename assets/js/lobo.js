26ed8ac148df6c2242e925b8fbc30f2de48fc563
Tomas Roa <tomas.roa@gmail.com>
2024-04-05 20:46:55 -0300

    se corrige archivo animal.js y se crea lobo.js

--- assets/js/lobo.js
+++ assets/js/lobo.js
@@ -1,5 +1,10 @@
 import animal from './animal.js';
 
 export class lobo extends animal{
-    
+    constructor(nombre, edad, img, comentarios, sonido){
+        super(nombre, edad, img, comentarios, sonido)
+    }
+aullar(){
+    document.getElementById(`auio${this.nombre}`).onplay();
+    }
 }
